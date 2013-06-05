7f10e70a28c21c309698c29b298237efe533f335
ketra <blech.kate@ya.ru>
2013-06-06 00:56:46 +0400

    error handling added

--- srv.py
+++ srv.py
@@ -1,35 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
-from flask import Flask, request, make_response
+import json, random 
+
+import logging, sys
+from flask import Flask, request, make_response 
 app = Flask(__name__)
 
+logging.basicConfig(stream=sys.stderr)
 
 def log_to_file( data ):
 	with open('/tmp/urls', 'w') as fp:
 		fp.write( json.dumps( data ) )
 
-def add( data ):
+def update( data ):
 	pass;
 
-def delete( data ):
+def remove( data ):
 	pass;
+
+def add_response_headers( headers={} ):
+	resp = make_response(f(*args, **kwargs))
+	h = resp.headers
+	for header, value in headers.items():
+		h[header] = value
+	print 'OK~'
+	return resp
 
 @app.route("/")
 def index():
 	action = request.args.get('action')
+	YPResponse = 1
+	msg = 'ok'
+	sid = random.getrandbits(32)  
 	if action == 'touch': 
-		log_to_file( request.args )
+		update( request.args )
 	elif action == 'add':
-		add( request.args )
+		log_to_file( request.args )
 	elif action == 'delete':
-		delete( request.args )
-		
+		remove( request.args )
+	else :
+		YPResponse = 0
+		msg = 'no action provided'
+		sid = -1
+				
 	response = make_response()
-	response.headers['YPResponse'] = 1
-	response.headers['YPMessage:'] = 'ok'
-	response.headers['SID'] = 1
+	response.headers['YPResponse'] = YPResponse
+	response.headers['YPMessage:'] = msg
+	response.headers['SID'] = sid
 	return response
 
 if __name__ == '__main__':
